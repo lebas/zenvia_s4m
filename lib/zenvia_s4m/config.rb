cd9d6a31ac0a3d35101adf615e1eade3c90e65e9
Eduardo Alencar <lebas66@gmail.com>
2015-08-27 18:07:39 -0300

    file config : fixed

--- lib/zenvia_s4m/config.rb
+++ lib/zenvia_s4m/config.rb
@@ -4,10 +4,10 @@ require 'singleton'
 module ZenviaS4m
 	class Config
 		include Singleton
-		@@timeout = @@code = @@account = nil
+		attr_accessor :timeout, :code, :account
 
 		def timeout 
-			@@timeout || 30
+			@timeout || 30
 		end
 	end
 end
--- lib/zenvia_s4m/version.rb
+++ lib/zenvia_s4m/version.rb
@@ -1,3 +1,3 @@
 module ZenviaS4m
-  VERSION = "0.1.6.1"
+  VERSION = "0.1.6.2"
 end
