48c7177963336ed9da40c5a39ff48f5567080e4e
Eduardo Alencar <lebas66@gmail.com>
2015-08-27 16:42:07 -0300

    configure : fixed

--- lib/zenvia_s4m.rb
+++ lib/zenvia_s4m.rb
@@ -1,8 +1,15 @@
 require "zenvia_s4m/version"
 
 module ZenviaS4m
-  # Your code goes here...
-  def self.ipsum
-  	" ... " 
+  def self.config 
+  	Config.instance
   end
+
+  def self.configure(&block)
+  	block.call(self.config)
+  end
+
+  autoload :Config, 'zenvia_s4m/config'
+  autoload :SendSms, 'zenvia_s4m/send_sms'
+  autoload :Version, 'zenvia_s4m/version' 
 end
